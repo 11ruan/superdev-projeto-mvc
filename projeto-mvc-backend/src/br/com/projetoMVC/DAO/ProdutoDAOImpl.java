0f8e75f34336e7abb3d9523313d18921be3791fa
Ruan lana <lanaruanpablo@gmail.com>
2024-03-15 22:09:00 -0300

    Feature: Listar Produtos por ID

--- projeto-mvc-backend/src/br/com/projetoMVC/DAO/ProdutoDAOImpl.java
+++ projeto-mvc-backend/src/br/com/projetoMVC/DAO/ProdutoDAOImpl.java
@@ -59,8 +59,37 @@ public class ProdutoDAOImpl implements GenericDAO {
 
 	@Override
 	public Object listarPorId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Produto produto = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM produto WHERE id = ?";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+				produto = new Produto();
+				produto.setId(rs.getInt("id"));
+				produto.setDescricao(rs.getString("descricao"));
+			}
+			
+		} catch (SQLException ex) {
+			System.out.println("Problema na DAO ao listar Produtos por id! " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				ConnectionFactory.closeConnection(conn, stmt, rs);
+			} catch (Exception e) {
+				System.out.println("Problema na DAO ao fechar Conexão! " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+ 		
+		
+		return produto;
 	}
 
 	@Override
